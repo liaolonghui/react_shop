6e1ffc6b5fea6c14aaecdb22e24dbc9f48da5d7c
liaolonghui <1340881930@qq.com>
2020-12-17 15:45:25 +0800

    header组件动态搭建

--- src/api/index.js
+++ src/api/index.js
@@ -1,5 +1,35 @@
 import ajax from './ajax'
+import jsonp from 'jsonp'
+import { message } from 'antd'
 
 export const reqLogin = (username, password) => ajax('/login', {username, password}, 'POST')
 
 export const reqAddUser = (user) => ajax('/manage/user/add', user, 'POST')
+
+
+// 天气查询          jsonp
+// 6c495fc6eeae6b603d48436aa7eb25f7
+// https://restapi.amap.com/v3/weather/weatherInfo?parameters       key city extensions output
+export const reqWeather = (city) => {
+
+  return new Promise((resolve, reject) => {
+    const url = `https://restapi.amap.com/v3/weather/weatherInfo?city=${city}&output=json&key=6c495fc6eeae6b603d48436aa7eb25f7`
+    jsonp(url, {}, (err, data)=> {
+      if (!err && data.status==='1') {
+        // 成功
+        const weather = data.lives[0].weather
+        let dayPicUrl = ''
+        if (weather === '晴') {
+          dayPicUrl = 'http://api.map.baidu.com/images/weather/day/qing.png'
+        } else {
+          dayPicUrl = 'http://api.map.baidu.com/images/weather/day/yin.png'
+        }
+        resolve({weather, dayPicUrl})
+      } else {
+        // 失败
+        message.error('获取天气信息失败！')
+      }
+    })
+  })
+
+}
--- src/components/header/index.jsx
+++ src/components/header/index.jsx
@@ -1,23 +1,80 @@
 import React, {Component} from 'react'
-import './index.less'
+import {withRouter} from 'react-router-dom'
 
-export default class Header extends Component {
+import './index.less'
+import menuList from '../../config/menuConfig'
+import { formateDate } from '../../utils/dateUtils'
+import memoryUtils from '../../utils/memoryUtils'
+import { reqWeather } from '../../api'
+
+class Header extends Component {
+
+  state = {
+    currentTime: formateDate(Date.now()), // 当前时间
+    dayPicUrl: '', // 天气图片
+    weather: '', // 天气文本
+  }
+
+  getTime = () => {
+    setInterval(()=>{
+      const currentTime = formateDate(Date.now())
+      this.setState({currentTime})
+    },1000)
+  }
+
+  getWeather = async () => {
+    const {dayPicUrl, weather} = await reqWeather('南昌')
+    this.setState({dayPicUrl, weather})
+  }
+
+  getTitle = () => {
+    const path = this.props.location.pathname
+    let title
+    // 从menuList中找出对应的title
+    menuList.forEach(item => {
+      if (item.key===path) {
+        title = item.title
+      } else if (item.children) {
+        const cItem = item.children.find(cItem => cItem.key===path)
+        if (cItem) {
+          title = cItem.title
+        }
+      }
+    })
+    return title
+  }
+
+  componentDidMount() {
+    // 第一次render之后执行一次，做异步操作
+    // 时间
+    this.getTime()
+    // 天气
+    this.getWeather()
+  }
+
   render() {
+
+    const { currentTime, dayPicUrl, weather } = this.state
+    const username = memoryUtils.user.username
+    const title = this.getTitle()
+
     return (
       <div className="header">
         <div className="header-top">
-          <span>欢迎，admin</span>
+          <span>欢迎，{username}</span>
           <a href="javascript(0);">退出</a>
         </div>
         <div className="header-bottom">
-          <div className="header-bottom-left">首页</div>
+          <div className="header-bottom-left">{title}</div>
           <div className="header-bottom-right">
-            <span>2019-5-16 10:12:36</span>
-            <img src="http://api.map.baidu.com/images/weather/day/qing.png" alt="weather" />
-            <span>晴</span>
+            <span>{currentTime}</span>
+            <img src={dayPicUrl} alt="weather" />
+            <span>{weather}</span>
           </div>
         </div>
       </div>
     )
   }
 }
+
+export default withRouter(Header)
