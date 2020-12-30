e94334b828ac9529289174e71b465d0116fe00a3
liaolonghui <1340881930@qq.com>
2020-12-30 20:51:03 +0800

    完成User组件的动态展示

--- src/api/index.js
+++ src/api/index.js
@@ -7,22 +7,21 @@ export const reqLogin = (username, password) => ajax('/login', {username, passwo
 // 添加用户
 export const reqAddUser = (user) => ajax('/manage/user/add', user, 'POST')
 
+
 // 获取一级/二级分类的列表
 export const reqCategorys = (parentId) => ajax('/manage/category/list', {parentId})
 // 添加分类
 export const reqAddCategory = ({categoryName, parentId}) => ajax('/manage/category/add', {categoryName, parentId}, 'POST')
 // 更新分类
 export const reqUpdateCagegory = ({categoryId, categoryName}) => ajax('/manage/category/update', {categoryId, categoryName}, 'POST')
-
 // 获取一个分类
 export const reqCategory = (categoryId) => ajax('/manage/category/info', {categoryId})
 
+
 // 获取商品分类列表
 export const reqProducts = (pageNum, pageSize) => ajax('/manage/product/list', {pageNum, pageSize})
-
 // 更新商品的状态（上架/下架）
 export const reqUpdateStatus = (productId, status) => ajax('/manage/product/updateStatus', {productId, status}, 'POST')
-
 // 搜索商品分类列表（根据商品名称/商品描述）
 // searchType的值是productName或productDesc
 export const reqSearchProducts = ({pageNum, pageSize, searchName, searchType}) => ajax('/manage/product/search', {
@@ -30,21 +29,23 @@ export const reqSearchProducts = ({pageNum, pageSize, searchName, searchType}) =
   pageSize,
   [searchType]: searchName
 })
-
 // 删除指定名称的图片
 export const reqDeleteImg = (name) => ajax('/manage/img/delete', {name}, 'POST')
-
 // 添加/修改商品（看传入的product是否有_id，有的话就是update，否则就是add）
 export const reqAddOrUpdateProduct = (product) => ajax('/manage/product/'+(product._id?'update':'add'), product, 'POST')
 
+
 // 获取所有角色的列表 
 export const reqRoles = () => ajax('/manage/role/list')
-
 // 添加角色
 export const reqAddRole = (roleName) => ajax('/manage/role/add', {roleName}, 'POST')
-
 // 更新角色（权限）
 export const reqUpdateRole = (role) => ajax('/manage/role/update', role, 'POST')
+
+
+// 获取所有用户的列表
+export const reqUsers = () => ajax('/manage/user/list')
+
 
 // 天气查询          jsonp
 // 6c495fc6eeae6b603d48436aa7eb25f7
--- src/pages/user/user.jsx
+++ src/pages/user/user.jsx
@@ -1,12 +1,104 @@
 import React, {Component} from 'react'
+import { Card, Button, Table, Modal } from 'antd'
+import { formateDate } from '../../utils/dateUtils'
+import { PAGE_SIZE } from '../../utils/constants'
+import { reqUsers } from '../../api'
 
 // 用户路由
 export default class User extends Component {
+
+  state = {
+    users: [],  //  所有的用户列表
+    roles: [],  // 所有的角色列表
+    isShow: false,  // 是否显示对话框
+  }
+
+  initColumns = () => {
+    this.columns = [
+      {
+        title: '用户名',
+        dataIndex: 'username'
+      },
+      {
+        title: '邮箱',
+        dataIndex: 'email'
+      },
+      {
+        title: '电话',
+        dataIndex: 'phone'
+      },
+      {
+        title: '注册时间',
+        dataIndex: 'create_time',
+        render: formateDate
+      },
+      {
+        title: '所属角色',
+        dataIndex: 'role_id',
+        render: (roleId) => this.roleName[roleId]
+      },
+      {
+        title: '操作',
+        render: (user) => (
+          <span>
+            <Button onClick={() => {this.setState({isShow: true})}}>修改</Button>
+            <Button style={{marginLeft: '5px'}}>删除</Button>
+          </span>
+        )
+      }
+    ]
+  }
+
+  // 初始化RoleName对象，提高查找效率。（一次遍历即可，不用再每次查找时都重新遍历了。）
+  initRoleName = (roles) => {
+    const RoleNames = roles.reduce((pre, role) => {
+      pre[role._id] = role.name
+      return pre
+    },{})
+    // 保存
+    this.roleName = RoleNames
+  }
+
+  // 获取所有的用户列表
+  getUsers = async () => {
+    const result = await reqUsers()
+    if (result.status === 0) {
+      const { users, roles } = result.data
+      // 初始化角色id为key，角色name为value的一个RoleName对象。
+      this.initRoleName(roles)
+      this.setState({
+        users,
+        roles
+      })
+    }
+  }
+
+  // 添加或更新用户
+  addOrUpdateUser = () => {
+
+  }
+
+  UNSAFE_componentWillMount() {
+    this.initColumns()
+  }
+
+  componentDidMount() {
+    this.getUsers()
+  }
+
   render() {
+
+    const { users, isShow } = this.state
+
+    const title = <Button type="primary" onClick={() => {this.setState({isShow: true})}}>创建用户</Button>
+
     return (
-      <div>
-        User
-      </div>
+      <Card title={title} >
+          <Table dataSource={users} columns={this.columns} pagination={{defaultPageSize: PAGE_SIZE}} loading={false} bordered rowKey="_id" />
+          <Modal title="添加用户" visible={isShow} onOk={this.addOrUpdateUser} onCancel={() => {this.setState({isShow: false})}} destroyOnClose>
+            <div>添加或删除用户</div>
+          </Modal>
+      </Card>
     )
   }
 }
